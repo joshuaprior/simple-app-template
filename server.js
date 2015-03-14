45ea51e171e670c817143daeff9ec5b465179b60
Josh Prior <joshuaprior@gmail.com>
2015-03-14 11:46:37 -0700

    Let's try this out
    Committing .dat file so I can test this out on a different computer.

--- server.js
+++ server.js
@@ -78,7 +78,36 @@ app.route('/rest/echo/:id')
             payload: req.body
         });
     });
+
+
+
+
+
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('data/sql_database.dat', function(err){
+    if(err) throw err;
     
+    db.serialize(function() {
+        /*db.run('DROP TABLE lorem');
+        db.run("CREATE TABLE lorem (info TEXT)");
+
+        var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+        for (var i = 0; i < 10; i++) {
+            stmt.run("Ipsum " + i);
+        }
+        stmt.finalize();
+        */
+
+        db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+            console.log(row.id + ": " + row.info);
+        });
+    });
+
+    db.close();
+});
+ 
+
+
 
 
 
